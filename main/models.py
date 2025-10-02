5ac77113e920ead9eab1af53683eb32e5a3fd076
18953660 <SRByadretdinov@sberbank.ru>
2025-10-02 23:37:41 +0200

    added templates

--- main/models.py
+++ main/models.py
@@ -21,7 +21,7 @@ class Size(models.Model):
         return self.name
     
 class ProductSize(models.Model):
-    product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='product_size')
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='product_sizes')
     size = models.ForeignKey('Size', on_delete=models.CASCADE)
     stock = models.PositiveIntegerField(default=0)
     
